33377d1255162ede492ce726d587ab1124225615
Andrey Érick <dev.andreyrk@gmail.com>
2021-03-14 12:19:28 -0300

    Update course_test.dart

--- test/course_test.dart
+++ test/course_test.dart
@@ -12,8 +12,7 @@ main() {
     for (dynamic asset in assets.keys) {
       if (asset.startsWith("rsc/courses/enem")) {
         final jsonObject = json.decode(await rootBundle.loadString(asset));
-        expect(jsonObject['id'] is String, equals(true));
-        expect(jsonObject['group'] is String, equals(true));
+        expect(jsonObject['id'] is String, equals(true
         expect(jsonObject['title'] is String, equals(true));
         expect(jsonObject['courses'] is List || jsonObject['classes'], equals(true));
       }
